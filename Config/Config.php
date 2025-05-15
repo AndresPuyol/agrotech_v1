7dc8fed8f8daad41c96eddea289cbb37333f1bfe
AndresPuyol <penaguzmanandresorlando@gmail.com>
2025-05-14 23:49:02 -0500

    subiendo una parte de mi crud y demas partes del api para hacer la fucion de las demas ramas al dev

---
+++ Config/Config.php
@@ -0,0 +1,18 @@
+<?php
+
+    //define("BASE_URL","http://localhost/championFramework/");
+    const BASE_URL = "http://localhost/championFramework4/";
+
+    //Datos de conexión a Base de Datos
+	const CONNECTION = true;
+	const DB_HOST = "localhost";
+	const DB_NAME = "apiproyect1.1";
+	const DB_USER = "root";
+	const DB_PASSWORD = "";
+	const DB_CHARSET = "utf8";
+
+
+
+
+
+?>
